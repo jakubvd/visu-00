e92ec60652698db33f12674061e35745b9bdd3b7
jakubvd <jakub.michalak@visiondevs.pl>
2025-08-01 01:47:56 +0200

    add current state to all

--- js/in-cms-filtr.js
+++ js/in-cms-filtr.js
@@ -2,9 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const filterButtons = document.querySelectorAll('.home-insights_category-filter-link');
   const collectionItems = document.querySelectorAll('.insigts-cms-collection-list .w-dyn-item');
 
+  // Na starcie dodajemy klasę 'current' do przycisku 'all'
+  const allButton = document.querySelector('.home-insights_category-filter-link[data-category="all"]');
+  if (allButton) {
+    allButton.classList.add('current');
+  }
+
   filterButtons.forEach(button => {
     button.addEventListener('click', () => {
-      // Usuwamy klasę 'current' z każdego przycisku
+      // Usuwamy klasę 'current' ze wszystkich przycisków
       filterButtons.forEach(btn => btn.classList.remove('current'));
       // Dodajemy klasę 'current' klikniętemu przyciskowi
       button.classList.add('current');
@@ -13,12 +19,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
       collectionItems.forEach(item => {
         const itemCategory = item.getAttribute('data-category');
-        
+
         // Jeśli 'all' to pokazujemy wszystko, inaczej filtrujemy
         if (filterValue === 'all' || itemCategory === filterValue) {
-          item.style.display = ''; // pokaz
+          item.style.display = '';
         } else {
-          item.style.display = 'none'; // ukryj
+          item.style.display = 'none';
         }
       });
     });
