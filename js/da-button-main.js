b69417467a85363daefff7e5d69104f36c54efc4
jakubvd <jakub.michalak@visiondevs.pl>
2025-08-02 00:14:37 +0200

    da for paddings sides

--- js/da-button-main.js
+++ js/da-button-main.js
@@ -11,3 +11,17 @@ document.querySelectorAll('[data-text-color]').forEach(el => {
     el.style.color = `var(${cssVarName})`;
   }
 });
+
+document.querySelectorAll('[data-padding-left]').forEach(el => {
+  const paddingValue = el.getAttribute('data-padding-left'); // np. "2rem"
+  if (paddingValue) {
+    el.style.paddingLeft = paddingValue;
+  }
+});
+
+document.querySelectorAll('[data-padding-right]').forEach(el => {
+  const paddingValue = el.getAttribute('data-padding-right'); // np. "1.5rem"
+  if (paddingValue) {
+    el.style.paddingRight = paddingValue;
+  }
+});
