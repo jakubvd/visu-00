ab18c6aa17cd8e0299afca48175bc1b2ff752504
jakubvd <jakub.michalak@visiondevs.pl>
2025-08-01 23:51:38 +0200

    da for main button text color - working

--- js/da-button-main.js
+++ js/da-button-main.js
@@ -1,7 +1,13 @@
 document.querySelectorAll('[data-bg-color]').forEach(el => {
   const cssVarName = el.getAttribute('data-bg-color'); // np. "--bg-color--button-main-bg-blue"
   if (cssVarName) {
-    // Ustawienie background-color z CSS variable
     el.style.backgroundColor = `var(${cssVarName})`;
+  }
+});
+
+document.querySelectorAll('[data-text-color]').forEach(el => {
+  const cssVarName = el.getAttribute('data-text-color'); // np. "--color--button-main-text-blue"
+  if (cssVarName) {
+    el.style.color = `var(${cssVarName})`;
   }
 });
